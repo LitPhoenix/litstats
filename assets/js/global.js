883ef14f94ec50781d5de9a8a2f3a9e59a11aca6
LitPhoenix <60115292+LitPhoenix@users.noreply.github.com>
2026-06-17 15:44:59 +0100

    Change banner mode from 'random' to 'emergency'

--- assets/js/global.js
+++ assets/js/global.js
@@ -151,7 +151,7 @@ const randomBanners = [
 ];
 
 // Toggle: 'main', 'emergency', 'random', or 'off'
-let bannerMode = 'random'; 
+let bannerMode = 'emergency'; 
 
 // Increment this string (e.g., 'v2', 'v3') to force the banner to reappear for users who closed an older one
 const bannerVersion = 'v1'; 
